e1ec3cc128acfa2df58f46e2b358b4af514cef96
Borges dev <leosull50@hotmail.com>
2022-10-03 15:01:29 -0300

    implements findById DTO

--- clienteCrud/src/main/java/com/study/spring/services/ClientService.java
+++ clienteCrud/src/main/java/com/study/spring/services/ClientService.java
@@ -1,13 +1,17 @@
 package com.study.spring.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.study.spring.dto.ClientDTO;
 import com.study.spring.entities.Client;
 import com.study.spring.repositories.ClientRepository;
+
 
 @Service
 public class ClientService {
@@ -16,7 +20,15 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();	
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.get();
+		return new ClientDTO(entity);
 	}
 }
