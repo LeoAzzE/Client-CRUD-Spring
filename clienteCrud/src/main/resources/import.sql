c5f7fb6d16e247cca5394db92cb9b77bbf26a1f8
Borges dev <leosull50@hotmail.com>
2022-10-03 12:57:13 -0300

    Database seeding

---
+++ clienteCrud/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_CLIENT(birth_Date, children, cpf, income, name) VALUES('2000-03-30', 2, '122.232.233-52', 2400, 'Maria porttinari');
+INSERT INTO TB_CLIENT(birth_Date, children, cpf, income, name) VALUES('1990-07-22', 1, '158.277.593-28', 9000, 'José Augusto');
+INSERT INTO TB_CLIENT(birth_Date, children, cpf, income, name) VALUES('2001-10-02', 4, '192.332.233-76', 15000, 'luiz mendes');
